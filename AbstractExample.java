157c39c5dc3dd8e9d75447134b3a39295cca3ebf
unknown <brishavdevnath@gmail.com>
2025-08-18 17:07:21 +0530

    testing after format

---
+++ AbstractExample.java
@@ -0,0 +1,20 @@
+abstract class Shape {
+    abstract void draw();
+}
+
+// Shape s = new Shape(); // Cannot instantiate abstract class
+class Circle extends Shape { // Need subchild class
+    void draw() {
+        System.out.println("Drawing Circle.");
+    }
+}
+// Shape s = new Circle();
+
+public class AbstractExample {
+    public static void main(String[] args) {
+        Shape s = new Circle();
+        s.draw();
+    }
+}
+
+//Hi, Just making some changes.
